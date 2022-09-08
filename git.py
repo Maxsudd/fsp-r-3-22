c4f603dd0ef571cdd084c319d8a24dca1377689f
Maxsud Fazilov <nmfazilov@gmail.com>
2022-09-08 20:17:51 +0500

    Add change to ssh

---
+++ git.py
@@ -0,0 +1,10 @@
+# ssh key generator
+# -t = type
+# -b = bits
+
+# ssh -T git@github.com
+# -T = tunnel connection to git@github.com
+
+#! public key = замочная скважина
+#? private key = ключ к замку
+
